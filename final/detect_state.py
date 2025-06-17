b284ce9578d5ca22b5781e6e975b226afa1d38e5
Nikola Szucsich <nikolaszu@gmail.com>
2025-06-17 13:07:43 +0200

    fix output format

--- final/detect_state.py
+++ final/detect_state.py
@@ -91,7 +91,9 @@ def get_cube_stack_state(cube_names, cube_sizes):
 while True:
     out = ""
     for line in get_cube_stack_state(CUBE_NAMES, CUBE_SIZES):
-        out += f"{line}\n"
+        for value in line:
+            out += f"{value},"
+        out = f"{out[:-1]}\n"
     with open("current_state.txt", "w+") as file:
         file.write(out)
     time.sleep(1)
